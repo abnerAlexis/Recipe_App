e41d53c1668752ff43bfd7eafe411fa53ce4bc59
Alexis Abner <alexiproj@gmail.com>
2024-09-11 20:44:40 -0600

    Switch debug to false

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -28,7 +28,8 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
