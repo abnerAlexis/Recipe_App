58ada5ee8ec0e3beaa15da12b84ac72d61d18ab0
Alexis Abner <alexiproj@gmail.com>
2024-09-11 19:06:50 -0600

    set static root for staticfiles in settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -120,9 +120,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
 
 
 MEDIA_URL = '/media/'
